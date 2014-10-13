68277353038034eec9ecd0f42cd26a554cb8847a
Vikram Ahuja <vikramahuja8803@gmail.com>
2014-10-13 22:11:49 +0530

    Comments removed

--- source/AndroidVideoCapture/src/com/exercise/AndroidVideoCapture/FormRead.java
+++ source/AndroidVideoCapture/src/com/exercise/AndroidVideoCapture/FormRead.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-
 public class FormRead extends Activity {
     String[] events = {"Birthday", "Wedding", "Engagement", "Reception", "House Warming", "Anniversary"};
     Button ContinueButton;
@@ -21,25 +20,17 @@ public class FormRead extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_read);
         setTitle("                   Event Details");
-  //      Intent intent = getIntent();
-    //    String message = intent.getStringExtra(StaggeredGridActivityFragment.EXTRA_MESSAGE);
         TextView t = (TextView)findViewById(R.id.eventName);
         ContinueButton = (Button)findViewById(R.id.continueEventDetails);
         ContinueButton.setOnClickListener(ContinueButtonOnClickListener);
-      //  t.setText(events[Integer.parseInt(message)]);
     }
 
     Button.OnClickListener ContinueButtonOnClickListener
     = new Button.OnClickListener(){
     
     	public void onClick(View v) {
-    		
-    		
     		Intent openvideocapture = new Intent("com.exercise.AndroidVideoCapture.ANDROIDVIDEOCAPTURE");
-    		
-			startActivity(openvideocapture);
-    		
- 
+			startActivity(openvideocapture); 
     	      
     	}
     };
