e14f9bb7c10b7ad433b5bbe09997f42c40878fdd
Jalpreet Singh Nanda <jalpreetsingh.nanda@students.iiit.ac.in>
2014-10-14 00:40:27 +0530

    Show Script Page Fully Functional

--- source/raj_MobileVideoInvitations/app/src/main/java/com/yourevent/mobilevideoinvitation/EnterEventDetails.java
+++ source/raj_MobileVideoInvitations/app/src/main/java/com/yourevent/mobilevideoinvitation/EnterEventDetails.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class EnterEventDetails extends Activity {
@@ -16,23 +17,32 @@ public class EnterEventDetails extends Activity {
     Button contButton;
     public String event;
     public final static String EXTRA = "";
-
+    EditText name;
+    EditText description;
+    EditText venue;
+    EditText date;
+    EditText time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_details);
-        setTitle("                   Event Details");
+        setTitle("Event Details");
         Intent intent = getIntent();
         String message = intent.getStringExtra(StaggeredGridActivityFragment.EXTRA_MESSAGE);
         event = intent.getStringExtra(StaggeredGridActivityFragment.EXTRA_MESSAGE);
         TextView t = (TextView)findViewById(R.id.eventName);
         t.setText(events[Integer.parseInt(message)]);
         contButton = (Button)findViewById(R.id.continueEventDetails);
+        name = (EditText) findViewById(R.id.etName);
+        description = (EditText) findViewById(R.id.etDescription);
+        venue = (EditText) findViewById(R.id.etVenue);
+        date = (EditText) findViewById(R.id.etDate);
+        time = (EditText) findViewById(R.id.etTime);
         contButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent("android.intent.action.STAGGEREDGRIDACTIVITY");
-                i.putExtra(EXTRA, events[Integer.parseInt(event)]);
+                i.putExtra(EXTRA, events[Integer.parseInt(event)]+" "+name.getText()+" "+description.getText()+" "+venue.getText()+" "+date.getText()+" "+time.getText());
                 startActivity(i);
             }
         });
