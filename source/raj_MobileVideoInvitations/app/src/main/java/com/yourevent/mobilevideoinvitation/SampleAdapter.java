40ac858a18bd7c17a0aacc20ac62ab7ec4675c1c
raj454raj <raj454raj@gmail.com>
2014-10-15 16:14:56 +0000

    Import error removed

--- source/raj_MobileVideoInvitations/app/src/main/java/com/yourevent/mobilevideoinvitation/SampleAdapter.java
+++ source/raj_MobileVideoInvitations/app/src/main/java/com/yourevent/mobilevideoinvitation/SampleAdapter.java
@@ -12,6 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import com.etsy.android.grid.util.DynamicHeightTextView;
+import android.graphics.Color;
+
 /***
  * ADAPTER
  */
