7ae74ca48115cd2c4b76e71404358657ec9d844d
raj454raj <raj454raj@gmail.com>
2014-11-11 18:12:34 +0530

    Images integrated in selectEvent screen

---
+++ source/Vinvite/app/src/main/java/com/yourevent/mobilevideoinvitation/Home.java
@@ -0,0 +1,117 @@
+package com.yourevent.mobilevideoinvitation;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.app.FragmentTransaction;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.yourevent.mobilevideoinvitation.adapter.TabsPagerAdapter;
+
+
+public class Home extends FragmentActivity implements ActionBar.TabListener {
+
+    private ViewPager viewPager;
+    private ActionBar actionBar;
+    private String[] tabs = {"Select Event", "Previous Invitations"};
+    SlidingMenu menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        //Initialization
+        actionBar = getActionBar();
+        assert actionBar != null;
+        //actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setTitle("Vinvite");
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        TabsPagerAdapter mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(mAdapter);
+        actionBar.setHomeButtonEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        menu = new SlidingMenu(this);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.menu);
+        getSupportFragmentManager().beginTransaction().replace(R.id.menu_frame, new UserMenu()).commit();
+        //Adding tabs
+        for (String tab_name: tabs){
+            actionBar.addTab(actionBar.newTab().setText(tab_name).setTabListener(this));
+        }
+
+        /**
+         * on swiping the viewpager make respective tab selected
+         * */
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                // on changing the page
+                // make respected tab selected
+                if(position == 0) {
+                    menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+                }
+                else{
+                    menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+                }
+                actionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+        actionBar.setSelectedNavigationItem(0);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction fragmentTransaction) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+}
